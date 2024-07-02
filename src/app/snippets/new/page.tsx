67ea6981613509f219fae5241ceae72d8d83beae
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-02 15:18:02 +0530

    feat: create a form to submit snippets

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,22 @@
+export default function SnippetCreatePage() {
+    return (
+        <form action="">
+            <h3 className='font-bold m-3'>Create a Snippet</h3>
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor="title">
+                        Title
+                    </label>
+                    <input className='border rounded p-2 w-full' id="title" name="title"/>
+                </div>
+                <div className='flex gap-4'>
+                    <label className='w-12' htmlFor="code">
+                        Code
+                    </label>
+                    <textarea className='border rounded p-2 w-full' id="code" name="code"/>
+                </div>
+                <button type="submit" className="rounded p-2 bg-blue-200">Save</button>
+            </div>
+        </form>
+    )
+}
