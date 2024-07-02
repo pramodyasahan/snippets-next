4e2be6a39829f7ee2b147f60bb13f7877c255ec5
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-03 00:25:49 +0530

    feat: implement show snippet page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
 import {db} from "@/db"
+import Link from "next/link";
 
 export default async function Home() {
     const snippets = await db.snippet.findMany();
     const renderedSnippets = snippets.map((snippet) => {
-        return(
-            <div key={snippet.id}>
-                {snippet.title}
+        return (
+            <div className="container">
+                <Link key={snippet.id}
+                      href={`/snippets/${snippet.id}`}
+                      className="flex justify-between items-center p-2 border rounded">
+                    <div>{snippet.title}</div>
+                    <div>Show</div>
+                </Link>
             </div>
         )
     })
 
     return (
         <div>
-            {renderedSnippets}
+            <div className="flex m-2 justify-between items-center">
+                <h1 className="text-xl font-bold">Snippets</h1>
+                <Link href="/snippets/new" className="border p-2 border-rounded">New</Link>
+            </div>
+            <div className="flex flex-col gap-2">{renderedSnippets}</div>
         </div>
     );
 }
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,18 @@
+import {notFound} from "next/navigation";
+import {db} from "@/db";
+
+interface SnippetShowPageProps {
+    params: {
+        id: string
+    }
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+    const snippet = await db.snippet.findFirst({
+        where: {id: parseInt(props.params.id)}
+    })
+
+    if (!snippet) return notFound()
+
+    return <div>Show a snippet {snippet.title}</div>
+}
