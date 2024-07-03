9de80a13723fb490d7c8c9c5c79dffa2b67f8eff
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-03 14:38:52 +0530

    feat: implement delete snippet logic

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,7 +6,13 @@ export async function editSnippet(id: number, code: string) {
     await db.snippet.update({
         where: {id},
         data: {code}
-    })
+    });
     redirect(`/snippets/${id}`);
 }
 
+export async function deleteSnippet(id: number) {
+    await db.snippet.delete({
+        where: {id}
+    });
+    redirect('/');
+}
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import {notFound} from "next/navigation";
 import {db} from "@/db";
 import Link from "next/link";
+import * as actions from "@/actions"
 
 interface SnippetShowPageProps {
     params: {
@@ -15,12 +16,16 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
     if (!snippet) return notFound()
 
+    const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
+
     return <div>
         <div className="flex m-4 justify-between items-center">
             <h1 className="text-2xl font-bold">{snippet.title}</h1>
             <div className="flex gap-4">
                 <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded-2xl px-6">Edit</Link>
-                <button className="p-2 border rounded-2xl px-6">Delete</button>
+                <form action={deleteSnippetAction}>
+                    <button className="p-2 border rounded-2xl px-6">Delete</button>
+                </form>
             </div>
         </div>
         <pre className="p-3 border rounded bg-gray-200 border-gray-300">
