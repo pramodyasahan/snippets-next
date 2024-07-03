b3819534be79309bcf20fa915e8b98994bbb6467
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-03 14:34:02 +0530

    feat: implement edit snippet page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
         <div>
             <div className="flex m-2 justify-between items-center">
                 <h1 className="text-xl font-bold">Snippets</h1>
-                <Link href="/snippets/new" className="border p-2 border-rounded">New</Link>
+                <Link href="/snippets/new" className="border p-2 bg-blue-50 rounded-2xl">New</Link>
             </div>
             <div className="flex flex-col gap-2">{renderedSnippets}</div>
         </div>
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import {notFound} from "next/navigation";
 import {db} from "@/db";
+import Link from "next/link";
 
 interface SnippetShowPageProps {
     params: {
@@ -14,5 +15,16 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
     if (!snippet) return notFound()
 
-    return <div>Show a snippet {snippet.title}</div>
+    return <div>
+        <div className="flex m-4 justify-between items-center">
+            <h1 className="text-2xl font-bold">{snippet.title}</h1>
+            <div className="flex gap-4">
+                <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded-2xl px-6">Edit</Link>
+                <button className="p-2 border rounded-2xl px-6">Delete</button>
+            </div>
+        </div>
+        <pre className="p-3 border rounded bg-gray-200 border-gray-300">
+            <code>{snippet.code}</code>
+        </pre>
+    </div>
 }
